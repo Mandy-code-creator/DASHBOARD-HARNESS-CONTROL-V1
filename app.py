55cf69946497a92546bd6fc59c75cfb987446b7c
Mandy-code-creator <i109143111@nkust.edu.tw>
2026-02-05 16:35:49 +0800

    Update app.py

--- app.py
+++ app.py
@@ -617,7 +617,8 @@ for _, g in valid.iterrows():
             pred_x = [coils[-1]+i+1 for i in range(len(hrb_values))]
             ax.scatter(pred_x, pred_values[prop], color="red", marker="X", s=80, label=f"{prop} Predicted")
             for j in range(len(hrb_values)):
-                ax.plot([coi]()
+            ax.plot([coils[-1], pred_x[j]], [sub_fit[prop].values[-1], pred_values[prop][j]], linestyle=":", color="red")
+
 
     elif view_mode == "📊 Hardness → Mechanical Range":
         st.markdown("## 📊 Hardness → Mechanical Properties Range")
