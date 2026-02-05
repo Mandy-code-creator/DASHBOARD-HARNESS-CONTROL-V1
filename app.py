172a6bb714e84d3bc4ee93ab3851db85af5f3c42
Mandy-code-creator <i109143111@nkust.edu.tw>
2026-02-05 11:02:50 +0800

    Update app.py

--- app.py
+++ app.py
@@ -175,7 +175,7 @@ view_mode = st.sidebar.radio(
         "🛠 Hardness → TS/YS/EL",
         "📊 TS/YS/EL Trend & Distribution",
         "🧮 Predict TS/YS/EL (Custom Hardness)",
-        "Hardness → Mechanical Range"
+        "📊 Hardness → Mechanical Range"
     ]
 )
 
